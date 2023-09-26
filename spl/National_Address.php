2de166de1e02553243a3dea12eea338b6c39bd95
alsobihi <alsobihi.ai@gmail.com>
2023-09-26 14:12:51 +0300

    clear

--- spl/National_Address.php
+++ spl/National_Address.php
@@ -8,12 +8,7 @@
 $json_url = "National_Address.json";
 $data = file_get_contents($json_url);
 $data = json_decode($data, false);
-var_dump(is_array($data));
 
-echo "<br>";
-echo $data[0]->title;
-echo "<br>";
-echo "<br>";
 
 foreach ($data[0] as $key => $value) {
     # code...
